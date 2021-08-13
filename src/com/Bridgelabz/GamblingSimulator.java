17eddc505b516b6aa7aef6da5ee29250f27da6d4
shivraj2806 <shivjadhav2806@gmail.com>
2021-08-13 08:37:40 +0530

    welcome massege

---
+++ src/com/Bridgelabz/GamblingSimulator.java
@@ -0,0 +1,7 @@
+package com.Bridgelabz;
+
+public class GamblingSimulator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to gambling simulator game");
+    }
+}
